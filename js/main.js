c3c76087329ccb4fdef194efd631c0a45b22ac05
Alex <kobzeysasha@gmail.com>
2021-12-10 17:03:28 +0200

    js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 document.getElementById("eng").addEventListener("click", function(){
     document.getElementById("eng").classList.add("active")
 });
+document.getElementById("ru").addEventListener("click", function(){
+    document.getElementById("ru").classList.add("active")
+});
