0de5566aeee2bdb7c1fc340a7fb8e8a25be05fcc
Alex <kobzeysasha@gmail.com>
2022-05-07 11:49:51 +0300

    new add

--- js/main.js
+++ js/main.js
@@ -1,39 +1,9 @@
-"use strict";
-
-document.getElementById('home-link').onclick = function() {
-    document.getElementById('home-link').classList.add('active');
-    document.getElementById('about-link').classList.remove('active');
-    document.getElementById('skills-link').classList.remove('active');
-    document.getElementById('portfolio-link').classList.remove('active');
-    document.getElementById('contacts-link').classList.remove('active');
-}
-document.getElementById('about-link').onclick = function() {
-    document.getElementById('home-link').classList.remove('active');
-    document.getElementById('about-link').classList.add('active');
-    document.getElementById('skills-link').classList.remove('active');
-    document.getElementById('portfolio-link').classList.remove('active');
-    document.getElementById('contacts-link').classList.remove('active');
-}
-document.getElementById('skills-link').onclick = function() {
-    document.getElementById('home-link').classList.remove('active');
-    document.getElementById('about-link').classList.remove('active');
-    document.getElementById('skills-link').classList.add('active');
-    document.getElementById('portfolio-link').classList.remove('active');
-    document.getElementById('contacts-link').classList.remove('active');
-}
-document.getElementById('portfolio-link').onclick = function() {
-    document.getElementById('home-link').classList.remove('active');
-    document.getElementById('about-link').classList.remove('active');
-    document.getElementById('skills-link').classList.remove('active');
-    document.getElementById('portfolio-link').classList.add('active');
-    document.getElementById('contacts-link').classList.remove('active');
-}
-document.getElementById('contacts-link').onclick = function() {
-    document.getElementById('home-link').classList.remove('active');
-    document.getElementById('about-link').classList.remove('active');
-    document.getElementById('skills-link').classList.remove('active');
-    document.getElementById('portfolio-link').classList.remove('active');
-    document.getElementById('contacts-link').classList.add('active');
+document.getElementById('more').onclick = function() {
+    document.getElementById('first').classList.toggle('active');
+    document.getElementById('second').classList.toggle('active');
 }
 
-
+document.getElementById('button').onclick = function() {
+    document.getElementById('list').classList.toggle('active');
+    document.getElementById('button').classList.toggle('active');
+}
